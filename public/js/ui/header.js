3713255d65078490ddcbff50ca0c1d0d3c66a363
gguip1 <gguip7554@naver.com>
2025-10-25 14:57:56 +0900

    feat: loadHeader 함수에서 async 코드 주석 제거

--- public/js/ui/header.js
+++ public/js/ui/header.js
@@ -7,13 +7,7 @@ function loadHeader(){
         .then(data => {
             document.querySelector('header').outerHTML = data;
         });
-} // Code Editor가 async를 추천하는데 왜 그런지는 잘 모르겠음
-
-// async function loadHeader(){
-//     const response = await fetch('/components/header.html');
-//     const data = await response.text();
-//     document.querySelector('header').outerHTML = data;
-// }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     loadHeader().
